42aa4afc06750650da40346b441223738af10988
Falko Schumann <falko.schumann@muspellheim.de>
2020-12-23 18:28:06 +0100

    Komponente Info-Footer angelegt

---
+++ src/portals/InfoFooter.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Meta } from "@storybook/react";
+
+import InfoFooter from "./InfoFooter";
+
+export default {
+  title: "Info Footer",
+  component: InfoFooter,
+} as Meta;
+
+export const Default = () => <InfoFooter />;
---
+++ src/portals/InfoFooter.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function InfoFooter() {
+  return (
+    <footer className="info">
+      <p>Double-click to edit a todo</p>
+      <p>
+        Created by <a href="https://github.com/falkoschumann/todomvc-typescript-react">Falko Schumann</a>
+      </p>
+      <p>
+        Part of <a href="http://todomvc.com">TodoMVC</a>
+      </p>
+    </footer>
+  );
+}
+
+export default InfoFooter;

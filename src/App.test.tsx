8f0692247000c1a4e3caaf4b20392ea286325cad
Falko Schumann <falko.schumann@muspellheim.de>
2021-01-12 16:25:06 +0100

    Snapshots korrigiert

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
 test('renders todos title', () => {
-  render(<App />);
+  render(<Router><App /></Router>);
   const titleElement = screen.getByText(/todos/i);
   expect(titleElement).toBeInTheDocument();
 });
